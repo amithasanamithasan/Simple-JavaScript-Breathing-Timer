ddc818c5434836d2c479decfe58544f21e516b02
amithasanamithasan <amit35-1773@diu.edu.bd>
2024-09-05 16:30:32 +0600

    setinterval stop korte hobe tar jonno likte hoi clearInterval setTimeout funcation use 3000 milisecound por punoriea abr Breath in start hobe  TimerDispaly restart

--- script.js
+++ script.js
@@ -4,11 +4,23 @@ const StopDisplay= document.getElementById('stop-btn')
 const startTimer=(time)=>{
 // console.log('start Timer Runnings')
 let timer= time;
-    setInterval(()=>{
+  const TimerInterval=  setInterval(()=>{
        const minutes= parseInt(timer / 60 , 10);
        const secound=parseInt(timer % 60 ,10)
 // console.log( 'timer rendaring 1scound '); 
 TimerDisplay.innerText=minutes +':'+secound;
+
+if(--timer<0){
+    // setinterval stop korte hobe tar jonno likte hoi clearInterval
+    clearInterval(TimerInterval);
+    TimerDisplay.innerText=('BREATH OUT');
+    // 3000 milisecound por punoriea abr Breath in start hobe 
+   setTimeout(()=>{
+    TimerDisplay.innerText='BREATH IN';
+
+   },3000)
+}
+
 },1000);
 
 }
