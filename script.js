1d22dec1b38e3c4ab04387098e8f4f78030484a9
amithasanamithasan <amit35-1773@diu.edu.bd>
2024-09-05 16:01:55 +0600

    Breathing simple javascript projet setup we use css html javascript and also button click addEventListener click then buton make event

---
+++ script.js
@@ -0,0 +1,11 @@
+const TimerDisplay= document.getElementById('timer')
+const StartDisplay= document.getElementById('start-btn')
+const StopDisplay= document.getElementById('stop-btn')
+
+StartDisplay.addEventListener('click',()=>{
+    console.log('start')
+})
+
+StopDisplay.addEventListener('click',()=>{
+    console.log('stops');
+})
