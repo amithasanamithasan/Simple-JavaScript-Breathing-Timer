3d41c95662d8619419d0748afd635470543a9e8f
amithasanamithasan <amit35-1773@diu.edu.bd>
2024-09-05 16:19:10 +0600

    timerDisplay minures and secound calculation and show display

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 const TimerDisplay= document.getElementById('timer')
 const StartDisplay= document.getElementById('start-btn')
 const StopDisplay= document.getElementById('stop-btn')
+const startTimer=(time)=>{
+// console.log('start Timer Runnings')
+let timer= time;
+    setInterval(()=>{
+       const minutes= parseInt(timer / 60 , 10);
+       const secound=parseInt(timer % 60 ,10)
+// console.log( 'timer rendaring 1scound '); 
+TimerDisplay.innerText=minutes +':'+secound;
+},1000);
 
+}
 StartDisplay.addEventListener('click',()=>{
-    console.log('start')
+    // console.log('start')
+    const timeLeft=90;
+    startTimer(timeLeft);
 })
 
 StopDisplay.addEventListener('click',()=>{
-    console.log('stops');
+    // console.log('stops');
 })
